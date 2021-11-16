963e304afa5b37704acedefa5c9a8f128eb8d741
Joshua Tonga <hellojoshuatonga@gmail.com>
2021-11-17 07:02:10 +0800

    feat: add install.sh script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# STOW_FOLDERS="gitconfig,tmux,vim,zsh"
+# STOW_FOLDERS="commitizen"
+# 
+# DOT_FILES=$HOME/dotfiles
+# 
+# pushd $DOT_FILES
+# for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+# do
+#     stow -D $folder
+#     stow $folder
+# done
+# popd
+
+stow commitizen
