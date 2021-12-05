51f5d278557c914aba17e892392db704ef1e8b27
Joshua Tonga <hellojoshuatonga@gmail.com>
2021-12-05 11:29:33 +0800

    feat: cleanup install.sh script

--- install.sh
+++ install.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-# STOW_FOLDERS="gitconfig,tmux,vim,zsh"
-# STOW_FOLDERS="commitizen"
-# 
-# DOT_FILES=$HOME/dotfiles
-# 
-# pushd $DOT_FILES
-# for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
-# do
-#     stow -D $folder
-#     stow $folder
-# done
-# popd
+STOW_FOLDERS="commitizen,tmux,configs,zsh,git,powerlevel10k"
 
-stow commitizen
-stow tmux
-stow configs
-stow zsh
-stow git
-stow powerlevel10k
+DOT_FILES=$HOME/dotfiles
+
+pushd $DOT_FILES
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+   stow -D $folder
+   stow $folder
+done
+popd
