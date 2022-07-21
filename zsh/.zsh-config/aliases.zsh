f681df7c2335bb422a06fa032082e0388685eb59
Joshua Tonga <hellojoshuatonga@gmail.com>
2022-07-21 13:19:32 +0800

    fix(zsh): pca alias not running all files

--- zsh/.zsh-config/aliases.zsh
+++ zsh/.zsh-config/aliases.zsh
@@ -18,7 +18,7 @@ alias f=fzf
 alias fb=firebase
 
 alias pc=pre-commit
-alias pca=pre-commit run --all-files
+alias pca="pre-commit run --all-files"
 
 alias vplug="vi $HOME/dotfiles/configs/.config/nvim/plugs/plugins.vim"
 alias aliases="vi $HOME/dotfiles/zsh/.zsh-config/aliases.zsh"
