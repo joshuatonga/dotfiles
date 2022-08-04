00b975d12c44e6364f7157d584983557aefaa523
Joshua Tonga <git@joshuatonga.com>
2022-08-04 15:08:37 +0800

    refactor(nvim): remove redundant noremap option

--- configs/.config/nvim/lua/joshuatonga/mappings.lua
+++ configs/.config/nvim/lua/joshuatonga/mappings.lua
@@ -4,7 +4,7 @@ local inoremap = keymap.inoremap
 
 vim.g.mapleader = ','
 
-nnoremap('Q', '<nop>',  { silent = true, noremap = true })
+nnoremap('Q', '<nop>',  { silent = true })
 nnoremap('<C-left>', ':tabprevious')
 nnoremap('<C-left>', ':tabprevious')
 nnoremap('<C-j>', '<C-w><C-j>')
@@ -12,10 +12,11 @@ nnoremap('<C-k>', '<C-w><C-k>')
 nnoremap('<C-l>', '<C-w><C-l>')
 nnoremap('<C-h>', '<C-w><C-h>')
 
-nnoremap('<C-w>+', ':resize +5<CR>', { silent = true, noremap = true })
-nnoremap('<C-w>-', ':resize -5<CR>', { silent = true, noremap = true })
-nnoremap('<C-w>>', ':vertical resize +5<CR>', { silent = true, noremap = true })
-nnoremap('<C-w><', ':vertical resize -5<CR>', { silent = true, noremap = true })
+print('mapping resizes')
+nnoremap('<C-w>+', ':resize +5<CR>', { silent = true })
+nnoremap('<C-w>-', ':resize -5<CR>', { silent = true })
+nnoremap('<C-w>>', ':vertical resize +5<CR>', { silent = true })
+nnoremap('<C-w><', ':vertical resize -5<CR>', { silent = true })
 
 nnoremap('<C-s', ':w<CR>')
 inoremap('<C-s', '<Esc>:w<CR>')
