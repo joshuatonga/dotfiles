30ebcd4022fbfc0dbeaa938fb89a06c8f3733884
Joshua Tonga <git@joshuatonga.com>
2022-08-05 00:24:44 +0800

    feat(nvim): add lsp terraformls lang server

--- configs/.config/nvim/lua/joshuatonga/lsp.lua
+++ configs/.config/nvim/lua/joshuatonga/lsp.lua
@@ -44,6 +44,7 @@ local lsp_flags = {
 }
 
 local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 lsp.sumneko_lua.setup {
   on_attach = on_attach,
   flags = lsp_flags,
@@ -77,6 +78,12 @@ lsp.pyright.setup {
 }
 
 lsp.tsserver.setup {
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+}
+
+lsp.terraformls.setup {
   on_attach = on_attach,
   flags = lsp_flags,
   capabilities = capabilities,
