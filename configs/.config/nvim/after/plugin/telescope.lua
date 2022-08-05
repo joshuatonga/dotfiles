db1eae74462d5b1a7cc8eb04d408105af6725633
Joshua Tonga <git@joshuatonga.com>
2022-08-05 14:44:52 +0800

    refactor(nvim): change mapping for telescope live_grep

--- configs/.config/nvim/after/plugin/telescope.lua
+++ configs/.config/nvim/after/plugin/telescope.lua
@@ -54,7 +54,7 @@ require('telescope').load_extension('fzf')
 vim.api.nvim_create_user_command('Maps', 'Telescope keymaps', {})
 
 nnoremap('<C-p>', ":lua require('telescope.builtin').find_files({ hidden = true })<CR>")
-nnoremap('<leader>fs', ":lua require('telescope.builtin').live_grep()<CR>")
+nnoremap('<leader>ff', ":lua require('telescope.builtin').live_grep()<CR>")
 nnoremap('<leader>fb', ":lua require('telescope.builtin').buffers()<CR>")
 nnoremap('<leader>fh', ":lua require('telescope.builtin').help_tags()<CR>")
 nnoremap('<leader>fm', ":lua require('telescope.builtin').man_pages()<CR>")
