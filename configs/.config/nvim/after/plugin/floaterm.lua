0dcaf48ba8b79e314d27a1333c7a4caeb902ec32
Joshua Tonga <git@joshuatonga.com>
2022-08-05 17:36:39 +0800

    fix(nvim): update mappings for vim-floaterm

--- configs/.config/nvim/after/plugin/floaterm.lua
+++ configs/.config/nvim/after/plugin/floaterm.lua
@@ -1,11 +1,17 @@
 local keymap = require 'joshuatonga.core.keymap'
 local nnoremap = keymap.nnoremap
+local tnoremap = keymap.tnoremap
 
-vim.g.floaterm_keymap_new = '<Leader>tf'
-vim.g.floaterm_keymap_toggle = '<Leader>tt'
-vim.g.floaterm_keymap_kill = '<Leader>tk'
-vim.g.floaterm_keymap_next = '<Leader>tn'
-vim.g.floaterm_keymap_prev = '<Leader>tp'
+nnoremap('<Leader>tf', ':FloatermNew<CR>')
+nnoremap('<Leader>tt', ':FloatermToggle<CR>')
+nnoremap('<Leader>tk', ':FloatermKill<CR>')
+nnoremap('<Leader>tn', ':FloatermNext<CR>')
+nnoremap('<Leader>tp', ':FloatermPrev<CR>')
+tnoremap('<Leader>tf', ':FloatermNew<CR>')
+tnoremap('<Leader>tt', ':FloatermToggle<CR>')
+tnoremap('<Leader>tk', ':FloatermKill<CR>')
+tnoremap('<Leader>tn', ':FloatermNext<CR>')
+tnoremap('<Leader>tp', ':FloatermPrev<CR>')
 
 vim.api.nvim_create_user_command('LG', 'FloatermNew --height=0.9 --width=0.9 lazygit', {})
 nnoremap('<Leader>gg', ':LG<CR>')
