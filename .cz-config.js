10033522538a77063c1aea94b66e01f082d7f738
Joshua Tonga <git@joshuatonga.com>
2022-08-05 15:32:10 +0800

    build(tools): add custom cz-config for this repo

---
+++ .cz-config.js
@@ -0,0 +1,84 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
+    {
+      value: 'style',
+      name: 'style:    Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+    },
+    {
+      value: 'refactor',
+      name: 'refactor: A code change that neither fixes a bug nor adds a feature',
+    },
+    {
+      value: 'perf',
+      name: 'perf:     A code change that improves performance',
+    },
+    { value: 'test', name: 'test:     Adding missing tests' },
+    {
+      value: 'chore',
+      name: 'chore:    Changes  to the auxiliary tools and libraries such as documentation generation',
+    },
+    {
+      value: 'build',
+      name: 'build:    Changes to the build process or auxiliary tools and libraries such as documentation generation',
+    },
+    { value: 'revert', name: 'revert:   Revert to a commit' },
+    { value: 'WIP', name: 'WIP:      Work in progress' },
+  ],
+
+  scopes: [
+    { name: 'nvim' },
+    { name: 'zsh' },
+    { name: 'tmux' },
+    { name: 'git' },
+    { name: 'font' },
+    { name: 'iterm2' },
+    { name: 'tools' },
+    { name: 'powerlevel10k' },
+    { name: 'color' },
+  ],
+
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+  scopeOverrides: {
+    fix: [
+
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },
+  */
+  // override the messages, defaults are as follows
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: '\nScope of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Scope of this change (optional):',
+    subject: 'Write a short, imperative tense description of the change:',
+    body: 'Provide a longer description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+  },
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['body', 'breaking', 'footer'],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+};
+
