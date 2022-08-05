b3dac1ddebd0474483297ab24f7313febcefad55
Joshua Tonga <git@joshuatonga.com>
2022-08-05 21:55:55 +0800

    refactor(nvim): add Rgh for ripgrep hidden

--- configs/.config/nvim/after/plugin/grepper.lua
+++ configs/.config/nvim/after/plugin/grepper.lua
@@ -1,1 +1,2 @@
-vim.api.nvim_create_user_command('RG', 'GrepperRg <args>', { nargs = '+' })
+vim.api.nvim_create_user_command('Rg', 'GrepperRg <args>', { nargs = '+' })
+vim.api.nvim_create_user_command('Rgh', 'GrepperRg --hidden <args>', { nargs = '+' })
