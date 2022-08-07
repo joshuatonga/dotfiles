0f87bb7265fa47e3667e0d930454549e06b09823
Joshua Tonga <git@joshuatonga.com>
2022-08-07 19:44:11 +0800

    feat(nvim): add mappings for common dev workflow

--- nvim/.config/nvim/after/plugin/asynctasks.lua
+++ nvim/.config/nvim/after/plugin/asynctasks.lua
@@ -1,3 +1,6 @@
+local keymap = require 'joshuatonga.core.keymap'
+local nnoremap = keymap.nnoremap
+
 vim.g.asyncrun_open = 6;  -- Open quickfix
 
 vim.api.nvim_create_user_command('Ar', 'AsyncRun <args>', {
@@ -8,3 +11,8 @@ vim.api.nvim_create_user_command('Ar', 'AsyncRun <args>', {
 vim.api.nvim_create_user_command('At', 'AsyncTask <args>', {
   nargs = '*'
 })
+
+local opts = { silent = true }
+nnoremap('<Leader>gab', ':AsyncRun build<CR>', opts)
+nnoremap('<Leader>gat', ':AsyncRun test<CR>', opts)
+nnoremap('<Leader>gar', ':AsyncRun run<CR>', opts)
