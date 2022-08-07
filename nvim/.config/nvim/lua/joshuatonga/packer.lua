20c48e8247f10282c0f44479b0a9a3ec336f4b38
Joshua Tonga <git@joshuatonga.com>
2022-08-07 14:29:10 +0800

    refactor(nvim): remove vim-snippets and just use user defined snippets

--- nvim/.config/nvim/lua/joshuatonga/packer.lua
+++ nvim/.config/nvim/lua/joshuatonga/packer.lua
@@ -42,7 +42,6 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
   use 'SirVer/ultisnips'
-  use 'honza/vim-snippets'
   use 'quangnguyen30192/cmp-nvim-ultisnips'
 
   -- Themes
