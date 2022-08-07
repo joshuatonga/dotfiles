fa45af3f2a142215357ccf14a50b9cbdb8f08443
Joshua Tonga <git@joshuatonga.com>
2022-08-07 22:25:23 +0800

    chore(nvim): change colorscheme to cinnabar

--- nvim/.config/nvim/lua/joshuatonga/theme.lua
+++ nvim/.config/nvim/lua/joshuatonga/theme.lua
@@ -1,3 +1,4 @@
-vim.g.tokyonight_style = "night"
-vim.cmd('colorscheme tokyonight')
+-- vim.g.tokyonight_style = "night"
+-- vim.cmd('colorscheme tokyonight')
+vim.cmd('colorscheme cinnabar')
 -- vim.cmd('colorscheme gruvbox')
