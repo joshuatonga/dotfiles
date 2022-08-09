154cce8f2eff3a11542a83ef752d230351ad8d79
Joshua Tonga <git@joshuatonga.com>
2022-08-09 11:42:29 +0800

    feat(nvim): add mappings for ultisnips

--- nvim/.config/nvim/after/plugin/ultisnips.lua
+++ nvim/.config/nvim/after/plugin/ultisnips.lua
@@ -1,6 +1,13 @@
+local keymap = require 'joshuatonga.core.keymap'
+local nnoremap = keymap.nnoremap
+
 vim.cmd [[
 augroup ultisnips_no_auto_expansion
     au!
     au VimEnter * au! UltiSnips_AutoTrigger
 augroup END
 ]]
+
+vim.g.UltiSnipsEditSplit = 'vertical'
+
+nnoremap('<Leader>use', ':UltiSnipsEdit<CR>')
