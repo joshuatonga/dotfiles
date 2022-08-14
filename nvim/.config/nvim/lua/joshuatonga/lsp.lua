f530fb917f02c425dce2fb54b6de72526dc7d147
Joshua Tonga <git@joshuatonga.com>
2022-08-15 00:25:44 +0800

    feat(nvim): add fix snippet terraform todo

--- nvim/.config/nvim/lua/joshuatonga/lsp.lua
+++ nvim/.config/nvim/lua/joshuatonga/lsp.lua
@@ -44,6 +44,7 @@ local lsp_flags = {
 }
 
 local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 local default_server_setup = {
   on_attach = on_attach,
   flags = lsp_flags,
@@ -79,6 +80,7 @@ lsp.sumneko_lua.setup {
 lsp.pyright.setup(default_server_setup)
 lsp.tsserver.setup(default_server_setup)
 
+-- TODO: Custom ultisnips not working
 lsp.terraformls.setup(default_server_setup)
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
   pattern = { '*.tf', '*.tfvars' },
