c24516415367a9129695ce29cda32368e70e55c1
Joshua Tonga <git@joshuatonga.com>
2022-08-18 21:46:55 +0800

    feat(nvim): add mapping for hop backward

--- nvim/.config/nvim/after/plugin/hop.lua
+++ nvim/.config/nvim/after/plugin/hop.lua
@@ -3,8 +3,14 @@ local keymap = require 'joshuatonga.core.keymap'
 local nnoremap = keymap.nnoremap
 
 nnoremap('<Leader><Leader>w', ':HopWord<CR>')
-nnoremap('f', "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>")
-nnoremap('f', "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>")
-nnoremap('F', "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>")
-nnoremap('t', "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<CR>")
-nnoremap('T', "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<CR>")
+nnoremap('<Leader><Leader>b', ':HopWordBC<CR>')
+nnoremap('f',
+  "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>")
+nnoremap('f',
+  "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>")
+nnoremap('F',
+  "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>")
+nnoremap('t',
+  "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<CR>")
+nnoremap('T',
+  "<CMD>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<CR>")
