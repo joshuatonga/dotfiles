d8fd2883148c0767ec064800da1220197dbfdae8
Joshua Tonga <git@joshuatonga.com>
2023-02-09 16:39:54 +0800

    chore(tmux): use default theme

--- zsh/.zsh-config/aliases.zsh
+++ zsh/.zsh-config/aliases.zsh
@@ -17,7 +17,7 @@ alias kt=kubetail
 alias alert="afplay /System/Library/Sounds/Submarine.aiff"
 
 alias t=tmux
-alias letsgo=tmux new-session -s c
+alias letsgo="tmux new -s c"
 alias tf=terraform
 alias g=git
 alias lg=lazygit
