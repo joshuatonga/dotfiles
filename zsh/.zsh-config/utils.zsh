af2147d1a1f4b3ec4bbcd0fdb4728af970b07be5
Joshua Tonga <git@joshuatonga.com>
2023-02-15 03:30:20 +0800

    feat(zsh): add ghpr function

--- zsh/.zsh-config/utils.zsh
+++ zsh/.zsh-config/utils.zsh
@@ -66,3 +66,7 @@ push-bashrc() {
   fi
   echo "export PS1=\"\\\u@\\h$_alias "\\W\$\""" | ssh $1 'mkdir -p .jtonga && cat > .jtonga/bashrc'
 }
+
+ghpr() {
+  GH_FORCE_TTY=100% gh pr list | fzf --ansi --preview 'GH_FORCE_TTY=100% gh pr view {1}' --preview-window down --header-lines 3 | awk '{print $1}' | xargs gh pr checkout
+}
