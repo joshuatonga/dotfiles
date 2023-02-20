899033b0dffc80a28ad0fe07cda3c607a6ea4764
Joshua Tonga <git@joshuatonga.com>
2023-02-20 20:09:42 +0800

    feat(nvim): add ansiblelint to null-ls

--- nvim/.config/nvim/lua/joshuatonga/lsp.lua
+++ nvim/.config/nvim/lua/joshuatonga/lsp.lua
@@ -18,7 +18,7 @@ nnoremap("<Space>q", vim.diagnostic.setloclist)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
 	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
