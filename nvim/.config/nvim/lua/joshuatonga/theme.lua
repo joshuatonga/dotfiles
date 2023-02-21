12df339de3bf8418cfd67107220abea7c715c9ad
Joshua Tonga <git@joshuatonga.com>
2023-02-21 11:36:26 +0800

    chore(nvim): use cinnabar theme

--- nvim/.config/nvim/lua/joshuatonga/theme.lua
+++ nvim/.config/nvim/lua/joshuatonga/theme.lua
@@ -7,15 +7,15 @@
 -- vim.cmd('colorscheme default')
 -- vim.cmd('colorscheme github_light')
 -- vim.cmd("colorscheme industry")
--- vim.cmd("colorscheme cinnabar")
+vim.cmd("colorscheme cinnabar")
 
 -- Example config in Lua
-require("github-theme").setup({
-	theme_style = "light",
-	function_style = "italic",
-	dark_float = true,
-	sidebars = { "qf", "vista_kind", "terminal", "packer" },
-
-	-- Change the "hint" color to the "orange" color, and make the "error" color bright red
-	colors = { hint = "orange", error = "#ff0000" },
-})
+-- require("github-theme").setup({
+--   theme_style = "light",
+--   function_style = "italic",
+--   dark_float = true,
+--   sidebars = { "qf", "vista_kind", "terminal", "packer" },
+--
+--   -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+--   colors = { hint = "orange", error = "#ff0000" },
+-- })
