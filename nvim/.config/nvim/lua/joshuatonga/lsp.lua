a3d3931eca5012db285363ab2733e9745dd609f0
Joshua Tonga <git@joshuatonga.com>
2023-03-06 23:47:11 +0800

    feat(nvim): add dockerls to lsp

--- nvim/.config/nvim/lua/joshuatonga/lsp.lua
+++ nvim/.config/nvim/lua/joshuatonga/lsp.lua
@@ -64,7 +64,7 @@ local default_server_setup = {
 	capabilities = capabilities,
 }
 
-local servers = { "gopls", "ansiblels", "bashls", "terraformls", "pyright", "rust_analyzer", "html" }
+local servers = { "gopls", "ansiblels", "bashls", "terraformls", "pyright", "rust_analyzer", "html", "dockerls" }
 
 for _, server in pairs(servers) do
 	lsp[server].setup(default_server_setup)
