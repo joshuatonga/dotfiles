526c8ddc0964bdcb49efe8807556dbb27fa2a1e8
Joshua Tonga <git@joshuatonga.com>
2023-10-18 16:29:02 +0800

    feat(nvim): update indent-blankline to version 3

--- nvim/.config/nvim/after/plugin/indent-blankline.lua
+++ nvim/.config/nvim/after/plugin/indent-blankline.lua
@@ -1,6 +1,1 @@
-require("indent_blankline").setup({
-	show_first_indent_level = false,
-	show_trailing_blankline_indent = false,
-	show_current_context = true,
-	show_current_context_start = false,
-})
+require("ibl").setup()
