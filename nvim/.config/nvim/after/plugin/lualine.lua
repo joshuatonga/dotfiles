8d85a7bd79c012700c64865b6a5c521c03582cc8
Joshua Tonga <git@joshuatonga.com>
2024-03-19 11:44:19 +0800

    improve lualine setup

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -1,9 +1,15 @@
+local custom_16color = require("lualine.themes.16color")
+
+custom_16color.normal.b.gui = "bold"
+custom_16color.inactive.a.bg = "#595959"
+custom_16color.inactive.a.fg = "#ffffff"
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = {},
+		theme = custom_16color,
+		component_separators = { left = "|", right = "|" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
@@ -20,7 +26,7 @@ require("lualine").setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
+		lualine_c = { { "filename", path = 3 } },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
@@ -28,13 +34,20 @@ require("lualine").setup({
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { "filename" },
+		lualine_c = { { "filename", path = 3 } },
 		lualine_x = { "location" },
 		lualine_y = {},
 		lualine_z = {},
 	},
-	tabline = {},
+	tabline = {
+		lualine_a = { { "tabs", mode = 2, path = 0 } },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = { { "filename", path = 2 }, "filesize" },
+		lualine_y = {},
+		lualine_z = {},
+	},
 	winbar = {},
 	inactive_winbar = {},
-	extensions = { "quickfix", "nerdtree" },
+	extensions = { "quickfix" },
 })
