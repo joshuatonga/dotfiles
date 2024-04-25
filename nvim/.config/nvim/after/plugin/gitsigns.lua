752e18998aebfb99968b4179d788962425eb818c
Joshua Tonga <git@jtonga.com>
2024-04-25 14:30:19 +0800

    reduce delay git line blame

--- nvim/.config/nvim/after/plugin/gitsigns.lua
+++ nvim/.config/nvim/after/plugin/gitsigns.lua
@@ -6,6 +6,13 @@ require("gitsigns").setup({
 		topdelete = { text = "‾" },
 		changedelete = { text = "~" },
 	},
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 100,
+		ignore_whitespace = false,
+		virt_text_priority = 100,
+	},
 	on_attach = function(bufnr)
 		local gs = package.loaded.gitsigns
 
