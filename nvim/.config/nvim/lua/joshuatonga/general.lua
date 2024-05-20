cc70cf5403cb8987cf5a096240b32f27a201d012
Joshua Tonga <git@jtonga.com>
2024-05-20 09:57:40 +0800

    cleanup config

--- nvim/.config/nvim/lua/joshuatonga/general.lua
+++ nvim/.config/nvim/lua/joshuatonga/general.lua
@@ -1,11 +1,3 @@
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = "python",
-	command = "setlocal ts=4 sw=4 sts=4 expandtab",
-})
-
-vim.api.nvim_set_hl(0, "LineNr", {})
-vim.api.nvim_set_hl(0, "NonText", { ctermbh = nil })
-
 vim.opt.confirm = true
 vim.opt.shortmess = vim.opt.shortmess + { A = true, c = true } -- disable swap message
 vim.opt.completeopt = { "menuone", "noselect", "noinsert" }
@@ -30,21 +22,17 @@ vim.opt.mat = 2 -- how many tenths of a second to blink when matching brackets
 vim.opt.number = true -- add left side numbes ( line number )
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2 -- size of a hard tabstop
-vim.opt.shiftwidth = 2 -- size of an indent
+vim.opt.shiftwidth = 2 -- size of an indent when shifting
 vim.opt.softtabstop = 2 -- a combination of spaces and tabs are used to simulate tab stops at a width other than the (hard)tabstop
 vim.opt.smarttab = true -- make tab insert indents instead of tabs at the beginning of a line
-vim.opt.expandtab = true -- always uses spaces instead of tab characters
-vim.opt.autoindent = true -- auto indent
-vim.opt.smartindent = true -- smart indent
-vim.opt.wrap = true -- wrap lines
-vim.opt.colorcolumn = "120"
+vim.opt.expandtab = true -- convert to spaces
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.colorcolumn = "80"
 vim.opt.foldmethod = "indent"
 vim.opt.foldnestmax = 10
 vim.opt.foldenable = false -- don't fold when opening a file
--- vim.opt.clipboard = { "unnamed", "unnamedplus" }
 vim.opt.shada = "!,'300,<50,@100,s10,h"
-vim.opt.mouse = ""
-vim.opt.scrolloff = 8
+vim.opt.mouse = "" -- disable mouse
 vim.g.netrw_bufsettings = "noma nomod nu nowrap ro nobl"
--- vim.g.netrw_altfile = 1
 vim.g.python3_host_prog = "/usr/bin/python"
