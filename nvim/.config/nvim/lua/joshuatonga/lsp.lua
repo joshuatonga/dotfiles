dc1fcc818fefa940fc88d896be517063434563a9
Joshua Tonga <git@jtonga.com>
2024-05-21 10:10:02 +0800

    fix deprecated diagnostic and completion

--- nvim/.config/nvim/lua/joshuatonga/lsp.lua
+++ nvim/.config/nvim/lua/joshuatonga/lsp.lua
@@ -22,16 +22,13 @@ nnoremap("<Space>q", vim.diagnostic.setloclist)
 
 vim.diagnostic.config({
 	float = {
-		source = "always",
+		source = true,
 	},
 })
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr)
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
