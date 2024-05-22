3281da03460c49d0fd46c665539d3dec770f3e92
Joshua Tonga <git@jtonga.com>
2024-05-22 15:22:47 +0800

    change theme to gruvbox

--- nvim/.config/nvim/lua/joshuatonga/theme.lua
+++ nvim/.config/nvim/lua/joshuatonga/theme.lua
@@ -2,8 +2,36 @@
 -- vim.cmd("colorscheme default")
 -- vim.cmd("colorscheme industry")
 -- vim.cmd("colorscheme ron")
-vim.cmd("colorscheme cinnabar")
+-- vim.cmd("colorscheme cinnabar")
+require("gruvbox").setup({
+	terminal_colors = true, -- add neovim terminal colors
+	undercurl = false,
+	underline = false,
+	bold = false,
+	italic = {
+		strings = false,
+		emphasis = false,
+		comments = false,
+		operators = false,
+		folds = false,
+	},
+	strikethrough = false,
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	invert_intend_guides = false,
+	inverse = true, -- invert background for search, diffs, statuslines and errors
+	contrast = "hard", -- can be "hard", "soft" or empty string
+	palette_overrides = {},
+	overrides = {},
+	dim_inactive = false,
+	transparent_mode = false,
+})
+vim.opt.background = "dark"
+vim.cmd("colorscheme gruvbox")
+vim.cmd("highlight Normal guibg=black")
 
+-- show a visual indicator when yanking
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup("HighlightYank", {})
