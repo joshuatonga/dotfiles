b7591409ea94b5aae286d09a4ae5ee2c7e593cb2
Joshua Tonga <git@jtonga.com>
2024-05-22 17:56:43 +0800

    remove ghost_text

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -41,9 +41,9 @@ cmp.setup({
 			vim.fn["UltiSnips#Anon"](args.body)
 		end,
 	},
-	experimental = {
-		ghost_text = true,
-	},
+	-- experimental = {
+	-- 	ghost_text = true,
+	-- },
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
