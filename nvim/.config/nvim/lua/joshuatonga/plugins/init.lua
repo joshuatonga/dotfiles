366e4c785d014256671edc177416afe1eaae44e8
Joshua Tonga <git@jtonga.com>
2024-05-23 09:44:14 +0800

    allow treesitter parser for diff

--- nvim/.config/nvim/lua/joshuatonga/plugins/init.lua
+++ nvim/.config/nvim/lua/joshuatonga/plugins/init.lua
@@ -37,7 +37,7 @@ return {
 					"rust",
 					"go",
 				},
-				ignore_install = { "diff" },
+				-- ignore_install = { "diff" },
 				-- Install parsers synchronously (only applied to `ensure_installed`)
 				sync_install = false,
 				-- Automatically install missing parsers when entering buffer
