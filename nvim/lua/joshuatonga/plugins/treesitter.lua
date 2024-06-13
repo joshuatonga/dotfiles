7584b78121b52a177fff777c62e5b440d7caed64
Joshua Tonga <git@jtonga.com>
2024-06-13 16:10:46 +0800

    add max_lines for context

--- nvim/lua/joshuatonga/plugins/treesitter.lua
+++ nvim/lua/joshuatonga/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
-		"nvim-treesitter/nvim-treesitter-context",
+		{ "nvim-treesitter/nvim-treesitter-context", opts = { max_lines = 2 } },
 	},
 	build = ":TSUpdate",
 	config = function()
