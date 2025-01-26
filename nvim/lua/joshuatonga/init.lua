43fc94d0a3e2fbf250ab4ea515d7326728aa62cb
Joshua Tonga <git@jtonga.com>
2025-01-26 13:52:18 +0800

    disable lazy checker

--- nvim/lua/joshuatonga/init.lua
+++ nvim/lua/joshuatonga/init.lua
@@ -27,7 +27,7 @@ require("lazy").setup({
 	-- colorscheme that will be used when installing plugins.
 	-- install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
-	checker = { enabled = true },
+	checker = { enabled = false },
 })
 
 require("joshuatonga.settings")
