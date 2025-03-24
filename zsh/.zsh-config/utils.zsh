8bd5763fe79986db1838fd29260716535fb28c80
Joshua Tonga <git@jtonga.com>
2025-03-25 00:39:22 +0800

    add ranger

--- zsh/.zsh-config/utils.zsh
+++ zsh/.zsh-config/utils.zsh
@@ -125,3 +125,19 @@ watch-api() {
     echo "$(date) - code: $(curl -s -o /dev/null -w "%{http_code}" $1 -m $sleepTimer)"; sleep $sleepTimer;
   done
 }
+
+function ranger {
+    local IFS=$'\t\n'
+    local tempfile="$(mktemp -t tmp.XXXXXX)"
+    local ranger_cmd=(
+        command
+        ranger
+        --cmd="map Q chain shell echo %d > "$tempfile"; quitall"
+    )
+
+    ${ranger_cmd[@]} "$@"
+    if [[ -f "$tempfile" ]] && [[ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]]; then
+        cd -- "$(cat "$tempfile")" || return
+    fi
+    command rm -f -- "$tempfile" 2>/dev/null
+}
