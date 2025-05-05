7fd069367c3299e17f5c931c7b67bead500d4ef7
Joshua Tonga <git@joshuatonga.com>
2025-05-05 23:44:01 +0800

    add oil.nvim

---
+++ nvim/lua/joshuatonga/plugins/oil.lua
@@ -0,0 +1,39 @@
+return {
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		default_file_explorer = false,
+
+		keymaps = {
+			["<M-p>"] = "actions.preview",
+			["<C-p>"] = false,
+
+			["gd"] = {
+				desc = "Toggle file detail view",
+				callback = function()
+					detail = not detail
+					if detail then
+						require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+					else
+						require("oil").set_columns({ "icon" })
+					end
+				end,
+			},
+		},
+
+		view_options = {
+			show_hidden = true,
+		},
+	},
+
+	config = function(_, opts)
+		require("oil").setup(opts)
+		vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
+	-- Optional dependencies
+	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+}
