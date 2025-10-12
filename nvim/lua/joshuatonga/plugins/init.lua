fac427d1adadc181f957677c410317e500d0b5a3
Joshua Tonga <git@joshuatonga.com>
2025-10-12 13:35:54 +0800

    add jsx auto close tag

--- nvim/lua/joshuatonga/plugins/init.lua
+++ nvim/lua/joshuatonga/plugins/init.lua
@@ -91,7 +91,16 @@ return {
 	{
 		"pmizio/typescript-tools.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {},
+		config = function()
+			require("typescript-tools").setup({
+				settings = {
+					jsx_close_tag = {
+						enable = true,
+						filetypes = { "javascriptreact", "typescriptreact" },
+					},
+				},
+			})
+		end,
 	},
 	{
 		"windwp/nvim-autopairs",
