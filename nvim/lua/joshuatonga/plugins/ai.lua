e0fec39abe0bccf6ea76100ad8efd681d0523f4e
Joshua Tonga <git@joshuatonga.com>
2025-10-21 23:30:25 +0800

    add codecompanion

--- nvim/lua/joshuatonga/plugins/ai.lua
+++ nvim/lua/joshuatonga/plugins/ai.lua
@@ -1,5 +1,20 @@
 return {
-  {
-    "github/copilot.vim",
-  },
+	{
+		"github/copilot.vim",
+	},
+
+	{
+		"olimorris/codecompanion.nvim",
+		opts = {
+			strategies = {
+				chat = {
+					adapter = "copilot",
+					model = "gpt-4.1",
+				},
+			},
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
 }
