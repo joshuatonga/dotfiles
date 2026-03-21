1745a0975af01d07811d83ba9a5f9f78eaa10fdb
Joshua Tonga <git@joshuatonga.com>
2026-03-21 14:52:40 +0800

    remove codecompanion.nvim

--- nvim/lua/joshuatonga/plugins/ai.lua
+++ nvim/lua/joshuatonga/plugins/ai.lua
@@ -2,19 +2,4 @@ return {
 	{
 		"github/copilot.vim",
 	},
-
-	{
-		"olimorris/codecompanion.nvim",
-		opts = {
-			strategies = {
-				chat = {
-					adapter = "copilot",
-					model = "gpt-4.1",
-				},
-			},
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
 }
