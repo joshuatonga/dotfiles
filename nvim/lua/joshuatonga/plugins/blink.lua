9f8a69c384f6218473d3189b609faf2ce5d27b8e
Joshua Tonga <git@joshuatonga.com>
2026-04-20 02:41:31 +0800

    add fuzzy file completion

--- nvim/lua/joshuatonga/plugins/blink.lua
+++ nvim/lua/joshuatonga/plugins/blink.lua
@@ -1,32 +1,51 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-	},
-
-	version = "*",
-	opts = {
-		keymap = { preset = "default" },
-
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
+	{
+		"saghen/blink.cmp",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
 		},
 
-		completion = {
-			menu = {
-				draw = {
-					columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+		version = "*",
+		opts = {
+			keymap = { preset = "default" },
+
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+
+			completion = {
+				menu = {
+					draw = {
+						columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
+					},
+				},
+				documentation = { auto_show = true, auto_show_delay_ms = 150 },
+			},
+
+			signature = { enabled = true },
+
+			sources = {
+				default = { "fuzzy-path", "lsp", "path", "snippets", "buffer" },
+				providers = {
+					["fuzzy-path"] = {
+						name = "Fuzzy Path",
+						module = "blink-cmp-fuzzy-path",
+						score_offset = 0,
+					},
 				},
 			},
-			documentation = { auto_show = true, auto_show_delay_ms = 150 },
 		},
+		opts_extend = { "sources.default" },
+	},
 
-		signature = { enabled = true },
-
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+	{
+		"newtoallofthis123/blink-cmp-fuzzy-path",
+		dependencies = { "saghen/blink.cmp" },
+		opts = {
+			filetypes = { "markdown", "json" },
+			trigger_char = "@",
+			max_results = 5,
 		},
 	},
-	opts_extend = { "sources.default" },
 }
