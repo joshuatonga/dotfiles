77be0ec33028ac369e7f6dd4d5116f8682a4d2fa
Joshua Tonga <git@joshuatonga.com>
2026-05-07 16:11:22 +0800

    fix GBrowse command

--- nvim/lua/joshuatonga/shame.lua
+++ nvim/lua/joshuatonga/shame.lua
@@ -1,1 +1,5 @@
 -- Don't know where to put it? put it here temporarily
+-- Fix for GBrowse too many arguments - https://github.com/tpope/vim-fugitive/issues/2441#issuecomment-4240457814
+vim.api.nvim_create_user_command("Browse", function(opts)
+	vim.fn.system({ "xdg-open", opts.fargs[1] })
+end, { nargs = 1 })
