1af3b4090813d817b22848b152bdb6a2c8301501
Joshua Tonga <git@joshuatonga.com>
2026-07-10 11:41:34 +0800

    switch back to dark theme

--- nvim/lua/joshuatonga/plugins/lualine.lua
+++ nvim/lua/joshuatonga/plugins/lualine.lua
@@ -11,7 +11,7 @@ return {
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = "auto",
+				theme = custom_16color,
 				component_separators = { left = "|", right = "|" },
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = {
--- nvim/lua/joshuatonga/theme.lua
+++ nvim/lua/joshuatonga/theme.lua
@@ -1,39 +1,39 @@
 vim.opt.background = "dark"
 
-require("cyberdream").setup({
-	borderless_pickers = false,
-
-	variant = "default",
-	highlights = {
-		LspInlayHint = { fg = "#176101" },
-		Comment = { fg = "#B2727B" },
-		LineNr = { fg = "#88C9F2" },
-		CursorLineNr = { fg = "#B2727B" },
-	},
-	colors = {
-		bg = "#000000",
-		bg_alt = "#1e2124",
-		bg_highlight = "#3c4048",
-		fg = "#ffffff",
-		grey = "#808080",
-		blue = "#88C9F2",
-		green = "#27c400",
-		cyan = "#0000E7",
-		red = "#f21616",
-		yellow = "#f1ff5e",
-		magenta = "#e81010",
-		pink = "#e81010",
-		orange = "#ed5615",
-		purple = "#52E2DD",
-	},
-})
-vim.cmd("colorscheme cyberdream")
+-- require("cyberdream").setup({
+-- 	borderless_pickers = false,
+--
+-- 	variant = "default",
+-- 	highlights = {
+-- 		LspInlayHint = { fg = "#176101" },
+-- 		Comment = { fg = "#B2727B" },
+-- 		LineNr = { fg = "#88C9F2" },
+-- 		CursorLineNr = { fg = "#B2727B" },
+-- 	},
+-- 	colors = {
+-- 		bg = "#000000",
+-- 		bg_alt = "#1e2124",
+-- 		bg_highlight = "#3c4048",
+-- 		fg = "#ffffff",
+-- 		grey = "#808080",
+-- 		blue = "#88C9F2",
+-- 		green = "#27c400",
+-- 		cyan = "#0000E7",
+-- 		red = "#f21616",
+-- 		yellow = "#f1ff5e",
+-- 		magenta = "#e81010",
+-- 		pink = "#e81010",
+-- 		orange = "#ed5615",
+-- 		purple = "#52E2DD",
+-- 	},
+-- })
+-- vim.cmd("colorscheme cyberdream")
 -- vim.cmd("colorscheme eldar")
 -- vim.cmd("colorscheme default")
--- vim.cmd("colorscheme industry")
+vim.cmd("colorscheme industry")
 -- vim.cmd("colorscheme ron")
 -- vim.cmd("colorscheme cinnabar")
-vim.cmd("colorscheme github_light_default")
+-- vim.cmd("colorscheme github_light_default")
 
 -- show a visual indicator when yanking
 vim.api.nvim_create_autocmd("TextYankPost", {
